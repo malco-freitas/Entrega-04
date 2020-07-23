16744d8b47589195a34b4f81bb738039cc694c83
Malco Freitas <malco.eletronica@poli.ufrj>
2020-07-22 23:49:48 -0300

    Resolvido problema com rota crashando em login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,21 +1,46 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import "./styles.css";
 
 class Login extends Component {
+    constructor() {
+        super();
+        this.state = {
+          login:"login",
+          senha:"senha",
+        };
+    }
     render() {
+        const {login,senha} = this.state;
         return(
+
             <div className="pagina">   
-                <div className="caixaLogin">
-                    <h1>Login</h1>
-                    <h2>Digite seu Login:</h2>
-                    <input type="text"/>
-                    <h2>Digite sua senha:</h2>
-                    <input type="password"/>
-                    <Login to={`/Inicio`}>
-                        <div className="botaoEntrar">Entrar</div>
-                    </Login>
-                </div>
-            </div>  
+            <div className="caixaCadastro">
+                <h1>Login</h1>
+                <h2>Digite seu Login:</h2>
+                <input type="text"/>
+                <div className="entradaLogin"></div>
+                <h2>Digite sua senha:</h2>
+                <input type="password"/>
+                
+                <Link to={`/Inicio`}>
+                    <div className="botaoLogin">Entrar</div>
+                </Link>
+            </div>
+        </div>     
+
+            // <div className="pagina">   
+            //     <div className="caixaLogin">
+            //         <h1>Login</h1>
+            //         <h2>Digite seu Login:</h2>
+            //         <input type="text"/>
+            //         <h2>Digite sua senha:</h2>
+            //         <input type="password"/>
+            //         <Login to={`/Inicio`}>
+            //             <div className="botaoEntrar">Entrar</div>
+            //         </Login>
+            //     </div>
+            // </div>  
         );
     }
 }
